cd193ac58c33905a15ce81d0b57196c2f48031b7
Miro Rauhala <mirorauhala@users.noreply.github.com>
2019-05-28 10:24:29 +0300

    update to bootstrap 4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,98 +1,45 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name') }}</title>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="{{ route('question.all') }}">{{ config('app.name') }}</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-    <style>
-
-    body.dragging, body.dragging * {
-        cursor: move !important;
-    }
-
-    .dragged {
-        position: absolute;
-        opacity: 0.5;
-        z-index: 2000;
-    }
-
-    .serialization {
-        list-style: none;
-        padding:0;
-    }
-    .serialization > li {
-        height: 34px;
-        padding: 5px 10px;
-        border: 1px solid #ddd;
-        background-color: whitesmoke;
-        margin-bottom: 5px;
-    }
-
-    .serialization > li.placeholder {
-        background: #f1f1f1;
-
-        border: 1px dotted #ddd;
-    }
-
-    </style>
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Onni Web Services') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        @if (Auth::check())
-                            <li {{ active(['question*']) }}><a href="{{ route('question.all') }}">Questions</a></li>
-                            <li {{ active(['category*']) }}><a href="{{ route('category.all') }}">Categories</a></li>
-                            <li {{ active(['settings*']) }}><a href="{{ route('settings.profile') }}">Settings</a></li>
-                        @endif
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item{{ active(['question*']) }}"><a class="nav-link" href="{{ route('question.all') }}">Questions</a></li>
+                        <li class="nav-item{{ active(['category*']) }}"><a class="nav-link" href="{{ route('category.all') }}">Categories</a></li>
+                        <li class="nav-item{{ active(['settings*']) }}"><a class="nav-link" href="{{ route('settings.profile') }}">Settings</a></li>
                     </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
+                    <ul class="navbar-nav">
                         @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li class="nav-item{{ active(['login*']) }}"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+                            <li class="nav-item{{ active(['register*']) }}"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
                         @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <li class="nav-item dropdown">
+                                <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
-                                    <li><a href="{{ route('settings.profile') }}">Settings</a></li>
-                                    <li>
+                                    <li class="dropdown-item"><a href="{{ route('settings.profile') }}">Settings</a></li>
+                                    <li class="dropdown-item">
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                                    document.getElementById('logout-form').submit();">
                                             Logout
                                         </a>
 
@@ -105,13 +52,11 @@
                         @endif
                     </ul>
                 </div>
-            </div>
-        </nav>
+            </nav>
 
-        @yield('content')
-    </div>
+            @yield('content')
+        </div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
 </html>
