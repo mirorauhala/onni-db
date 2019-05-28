e3a69c314843ab8fce622cac91485694574c0582
Miro Rauhala <mirorauhala@users.noreply.github.com>
2019-05-28 12:41:10 +0300

    bs4 menu

--- resources/views/partials/categories-menu.blade.php
+++ resources/views/partials/categories-menu.blade.php
@@ -1,10 +1,10 @@
 <div class="col-md-4">
-    <ul class="nav nav-pills nav-stacked">
-        <li {{ active(['category.all*']) }}>
-            <a href="{{ route('category.all') }}">List all</a>
+    <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+            <a class="nav-link {{ active(['category.all*']) }}" href="{{ route('category.all') }}">List all</a>
         </li>
-        <li {{ active(['category.new*']) }}>
-            <a href="{{ route('category.new') }}">Add new category</a>
+        <li class="nav-item">
+            <a class="nav-link {{ active(['category.new*']) }}" href="{{ route('category.new') }}">Add new category</a>
         </li>
     </ul>
 </div>
--- resources/views/partials/questions-menu.blade.php
+++ resources/views/partials/questions-menu.blade.php
@@ -1,10 +1,10 @@
 <div class="col-md-4">
-    <ul class="nav nav-pills nav-stacked">
-        <li {{ active(['question.all*']) }}>
-            <a href="{{ route('question.all') }}">List all</a>
+    <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+            <a class="nav-link{{ active(['question.all*']) }}" href="{{ route('question.all') }}">List all</a>
         </li>
-        <li {{ active(['question.new*']) }}>
-            <a href="{{ route('question.new') }}">Add new question</a>
+        <li class="nav-item">
+            <a class="nav-link{{ active(['question.new*']) }}" href="{{ route('question.new') }}">Add new question</a>
         </li>
     </ul>
 </div>
