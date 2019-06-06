767afcd26efb4d5ce972365305614a1886df71a3
Miro Rauhala <mirorauhala@users.noreply.github.com>
2019-06-06 09:10:47 +0300

    rework testing for questions

--- tests/Feature/QuestionCreateTest.php
+++ tests/Feature/QuestionCreateTest.php
@@ -13,18 +13,16 @@ class QuestionCreateTest extends TestCase
 {
     use DatabaseMigrations;
 
-    /**
-     * Test user can create a question.
-     *
-     * @return void
-     */
-    public function test_user_can_create_question()
-    {
-        $user = factory(User::class)->create();
-        $category = factory(Category::class)->create();
+    protected $payload;
+    protected $user;
+    protected $category;
 
-        $payload = [
-            'category' => $category->id,
+    public function setUp() : void {
+        parent::setUp();
+        $this->user = factory(User::class)->create();
+        $this->category = factory(Category::class)->create();
+        $this->payload = [
+            'category' => $this->category->id,
             'question' => 'This is a question?',
             'difficulty' => 1,
             'explanation' => 'This is the explanation.',
@@ -33,11 +31,19 @@ class QuestionCreateTest extends TestCase
             'answer3' => 'This is the answer three.',
             'answer4' => 'This is the answer four.'
         ];
+    }
 
-        $response = $this->actingAs($user)->post(route('question.store'), $payload);
+    /**
+     * Test user can create a question.
+     *
+     * @return void
+     */
+    public function test_user_can_create_question()
+    {
+        $response = $this->actingAs($this->user)->post(route('question.store'), $this->payload);
 
         $response->assertStatus(302);
-        $this->assertDatabaseHas('questions', Arr::except($payload, ['category']));
+        $this->assertDatabaseHas('questions', Arr::except($this->payload, ['category']));
     }
 
     /**
@@ -47,22 +53,124 @@ class QuestionCreateTest extends TestCase
      */
     public function test_question_needs_category()
     {
-        $user = factory(User::class)->create();
+        $payload = Arr::except($this->payload, ['category']);
 
-        $payload = [
-            'question' => 'This is a question?',
-            'difficulty' => 1,
-            'explanation' => 'This is the explanation.',
-            'answer1' => 'This is the answer one.',
-            'answer2' => 'This is the answer two.',
-            'answer3' => 'This is the answer three.',
-            'answer4' => 'This is the answer four.'
-        ];
-
-        $response = $this->actingAs($user)->post(route('question.store'), $payload);
+        $response = $this->actingAs($this->user)->post(route('question.store'), $payload);
 
         $response->assertStatus(302);
         $response->assertSessionHasErrors('category');
         $this->assertDatabaseMissing('questions', $payload);
+    }
+
+    /**
+     * Question is a required field.
+     *
+     * @return void
+     */
+    public function test_question_needs_question()
+    {
+        $payload = Arr::except($this->payload, ['question']);
+
+        $response = $this->actingAs($this->user)->post(route('question.store'), $payload);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('question');
+        $this->assertDatabaseMissing('questions', Arr::except($payload, ['category']));
+    }
+
+    /**
+     * Difficulty is a required field.
+     *
+     * @return void
+     */
+    public function test_question_needs_difficulty()
+    {
+        $payload = Arr::except($this->payload, ['explanation']);
+
+        $response = $this->actingAs($this->user)->post(route('question.store'), $payload);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('explanation');
+        $this->assertDatabaseMissing('questions', Arr::except($payload, ['category']));
+    }
+
+    /**
+     * Explanation is a required field.
+     *
+     * @return void
+     */
+    public function test_question_needs_explanation()
+    {
+        $payload = Arr::except($this->payload, ['explanation']);
+
+        $response = $this->actingAs($this->user)->post(route('question.store'), $payload);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('explanation');
+        $this->assertDatabaseMissing('questions', Arr::except($payload, ['category']));
+    }
+
+    /**
+     * Answer1 is a required field.
+     *
+     * @return void
+     */
+    public function test_question_needs_answer1()
+    {
+        $payload = Arr::except($this->payload, ['answer1']);
+
+        $response = $this->actingAs($this->user)->post(route('question.store'), $payload);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('answer1');
+        $this->assertDatabaseMissing('questions', Arr::except($payload, ['category']));
+    }
+
+    /**
+     * Answer2 is a required field.
+     *
+     * @return void
+     */
+    public function test_question_needs_answer2()
+    {
+        $payload = Arr::except($this->payload, ['answer2']);
+
+        $response = $this->actingAs($this->user)->post(route('question.store'), $payload);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('answer2');
+        $this->assertDatabaseMissing('questions', Arr::except($payload, ['category']));
+    }
+
+    /**
+     * Answer3 is a required field.
+     *
+     * @return void
+     */
+    public function test_question_needs_answer3()
+    {
+        $payload = Arr::except($this->payload, ['answer3']);
+
+        $response = $this->actingAs($this->user)->post(route('question.store'), $payload);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('answer3');
+        $this->assertDatabaseMissing('questions', Arr::except($payload, ['category']));
+    }
+
+    /**
+     * Answer4 is a required field.
+     *
+     * @return void
+     */
+    public function test_question_needs_answer4()
+    {
+        $payload = Arr::except($this->payload, ['answer4']);
+
+        $response = $this->actingAs($this->user)->post(route('question.store'), $payload);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('answer4');
+        $this->assertDatabaseMissing('questions', Arr::except($payload, ['category']));
     }
 }
