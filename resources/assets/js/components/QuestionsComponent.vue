58a989203df3b4f5d8abbf2a12460e895ba4d6fb
Miro Rauhala <mirorauhala@users.noreply.github.com>
2019-06-11 12:36:47 +0300

    add table filtering

---
+++ resources/assets/js/components/QuestionsComponent.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+
+        <form>
+            <b-form-input v-model="search" placeholder="Enter your name"></b-form-input>
+        </form>
+        
+        <b-table 
+            hover
+            :items="items"
+            :fields="fields"
+            :sort-by.sync="sortBy"
+            :sort-desc.sync="sortDesc"
+            :filter="search">
+
+            <template slot="is_enabled" slot-scope="row">
+                {{ row.value ? 'Yes' : 'No' }}
+            </template>
+
+            <template slot="actions" slot-scope="row">
+                <b-button size="sm" @click="edit(row.item, row.index, $event.target)" class="px-4 mr-1">
+                    Edit
+                </b-button>
+            </template>
+        </b-table>
+
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                sortBy: 'updated_at',
+                sortDesc: true,
+                items: [],
+                search: '',
+                fields: [
+                    { key: 'id', sortable: true},
+                    { key: 'question', sortable: true},
+                    { key: 'difficulty', sortable: true},
+                    { key: 'is_enabled', sortable: true},
+                    { key: 'category_id', sortable: true},
+                    { key: 'created_at', sortable: true},
+                    { key: 'updated_at', sortable: true},
+                    { key: 'actions', label: 'Actions'},
+                ]
+            }
+        },
+        mounted() {
+            this.items = window.Questions.data;
+        },
+        methods: {
+            edit(item, index, button) {
+                console.log(item)
+            }
+        }
+    }
+</script>
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,39 +5,11 @@
     <div class="row">
         <div class="col-12">
             @if(count($questions))
-                {{ $questions->links() }}
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Question</th>
-                            <th>Difficulty</th>
-                            <th>Is enabled?</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($questions as $question)
-                            <tr class="cursor-pointer">
-                                <td>{{ $question->id }}</td>
-                                <td>{{ $question->question }}</td>
-                                <td>{{ $question->difficulty }}</td>
-                                <td>{{ $question->is_enabled ? 'Yes' : 'No' }}</td>
-                                <td>
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Actions <span class="caret"></span>
-                                        </button>
-                                        <ul class="dropdown-menu">
-                                            <li><a class="dropdown-item" href="{{ route('question.edit', ['question' => $question->id]) }}">Edit</a></li>
-                                            <li><a class="dropdown-item" href="{{ route('question.delete', ['question' => $question->id]) }}">Delete</a></li>
-                                        </ul>
-                                    </div>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                <!-- TODO: use API -->
+                <script>
+                    window.Questions = {!! $questions->toJSON() !!}
+                </script>
+                <o-questions></o-questions>
                 {{ $questions->links() }}
             @else
                 <p>There are no questions.</p>
