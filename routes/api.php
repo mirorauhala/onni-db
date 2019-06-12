1083e2fd0ab0a83a2e69c22048205abb130c1f5e
Miro Rauhala <mirorauhala@users.noreply.github.com>
2019-06-12 12:46:42 +0300

    point to newer documentation

--- routes/api.php
+++ routes/api.php
@@ -2,41 +2,9 @@
 
 use Illuminate\Http\Request;
 
-/*
-
-/api/v1/questionId/{difficulty}/{category}
-    {difficulty} - Can be 1 - 3
-    {category} - Id of the category from which questions will be drawn
-
-Returns ID of the question that matches the category and difficulty
-
---
-
-/api/v1/question/{question}
-    {question} - Id of the question
-
-Returns the text form of question.
-
---
-
-/api/v1/answer/{question}/{number}
-    {question} - Id of the question
-    {number} - Which answer, can be 1 - 4.
-
-Returns the Nth answer for a question.
-
---
-
-/api/v1/explanation/{question}
-    {question} - Id of the question
-
-Returns the answer explanation.
-
---
-
-Psst. I didn't design the logic. Don't blame me.
-
-*/
+/**
+ * For documentation see README.md and ./docs
+ */
 
 Route::prefix('v1')->group(function () {
     Route::get('questionId/{difficulty}/{category}', 'Endpoints\EndpointOne@getQuestionId')->name('api.v1.getQuestionId');
